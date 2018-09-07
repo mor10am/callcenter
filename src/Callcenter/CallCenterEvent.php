6d8bafe35a3332f77081bbd02318a4345d3b6086
Morten Amundsen <morten.amundsen@no.teleperformance.com>
2018-09-07 12:09:21 +0200

    Define file as strict and fix up hints and docblock

--- src/Callcenter/CallCenterEvent.php
+++ src/Callcenter/CallCenterEvent.php
@@ -1,24 +1,42 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Callcenter;
 
 class CallCenterEvent
 {
+    /**
+     * @var string
+     */
     private $type;
+
+    /**
+     * @var array
+     */
     private $data;
 
+    /**
+     * @param string $type
+     * @param array $data
+     */
     public function __construct(string $type, array $data)
     {
         $this->type = $type;
         $this->data = $data;
     }
 
-    public function getType()
+    /**
+     * @return string
+     */
+    public function getType() : string
     {
         return $this->type;
     }
 
-    public function __get($field)
+    /**
+     * @param string $field
+     * @return mixed
+     */
+    public function __get(string $field)
     {
         if (!isset($this->data[$field])) {
             throw new \OutOfBoundsException("Field $field not found in event");
